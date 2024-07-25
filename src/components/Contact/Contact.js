d8e9f629c3a70c30c82d0f8d460a22f3c0bbab85
vijaya laxmi <159887996+vijaya-laxmi90@users.noreply.github.com>
2024-07-25 11:35:45 +0530

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -46,7 +46,7 @@ function Contact() {
 
         <div class="contactItem">
             
-            <div class="info"><a href="https://www.linkedin.com/in/vijayalaxmi90/" target="_blank">Linedin</a></div>
+            <div class="info"><a href="https://www.linkedin.com/in/vijayalaxmi90/" target="_blank">Linkedin</a></div>
         </div>
 
         <div class="contactItem">
